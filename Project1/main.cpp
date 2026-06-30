cb4dd72b20470fca92e4b3a08af6fbd8b65f7eb0
Avalon <2044786840@qq.com>
2026-07-01 00:32:22 +0800

    上传project文件夹

---
+++ Project1/main.cpp
@@ -0,0 +1,58 @@
+#include"stdafx.h"
+#include"Game.h"
+
+void Game::Start(void)
+{
+	if (_gameState != Uninitialized)
+		return;
+
+	_mainWindow.create(sf::VideoMode({1024, 768},32), "Project1");
+	_gameState = Game::Playing;
+
+	while (!IsExiting())
+	{
+		GameLoop();
+	}
+
+	_mainWindow.close();
+}
+
+bool Game::IsExiting()
+{
+	if (_gameState == Game::Exiting)
+		return 1;
+	else
+		return 0;
+}
+
+void Game::GameLoop()
+{
+	while (const std::optional event = Game::_mainWindow.pollEvent())
+	{
+		switch (_gameState)
+		{
+	    	case Game::Playing:
+    		{
+			    _mainWindow.clear(sf::Color(0,0,255));
+				_mainWindow.display();
+
+				if (event->is<sf::Event::Closed>())
+				{
+					_gameState = Game::Exiting;
+				}
+				break;
+			}
+		}
+	}
+
+}
+
+Game::GameState Game::_gameState = Uninitialized;
+sf::RenderWindow Game::_mainWindow;
+
+int main(int argc,char** argv)
+{
+	Game::Start();
+
+	return 0;
+}
