fcbad848267d3abf40b421c05fccf759dbe441a3
YuWenkai <38620252205176@stu.xmu.edu.cn>
2026-07-07 10:36:48 +0800

    Add UI healthbar expbar package

---
+++ ui_healthbar_expbar_package/clean/UI.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "SFML/Graphics.hpp"
+
+class Player;
+class Enemy;
+
+class StatusBar
+{
+public:
+    StatusBar();
+
+    void SetSize(float width, float height);
+    void SetPosition(float x, float y);
+    void SetColors(sf::Color backgroundColor, sf::Color fillColor);
+    void SetValue(float currentValue, float maxValue);
+    void Draw(sf::RenderWindow& window);
+
+private:
+    sf::RectangleShape _background;
+    sf::RectangleShape _fill;
+
+    float _width = 100.f;
+    float _height = 10.f;
+    float _ratio = 1.f;
+};
+
+class UIManager
+{
+public:
+    UIManager();
+
+    void DrawPlayerHUD(sf::RenderWindow& window, const Player* player);
+    void DrawEnemyHealthBar(sf::RenderWindow& window, const Enemy* enemy);
+
+private:
+    StatusBar _playerHealthBar;
+    StatusBar _playerExperienceBar;
+    StatusBar _enemyHealthBar;
+};
